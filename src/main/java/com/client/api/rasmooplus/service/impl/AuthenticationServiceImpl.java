6bcb7a22b1d643fa7c2ded0fcc708fc5f5a48d7b
Felipe Alves <felipe.barbosa@ext.valid.com>
2024-05-03 00:15:54 -0300

    Post keycloak Login by Spring

--- src/main/java/com/client/api/rasmooplus/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/client/api/rasmooplus/service/impl/AuthenticationServiceImpl.java
@@ -1,12 +1,16 @@
 package com.client.api.rasmooplus.service.impl;
 
 import com.client.api.rasmooplus.component.HttpComponent;
+import com.client.api.rasmooplus.dto.KeycloakOAuthDto;
 import com.client.api.rasmooplus.dto.LoginDto;
 import com.client.api.rasmooplus.exception.BadRequestException;
 import com.client.api.rasmooplus.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -29,7 +33,20 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public String auth(LoginDto dto) {
         try {
-            return "";
+            MultiValueMap<String, String> keycloakOAuth = KeycloakOAuthDto.builder()
+                    .clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .username(dto.getUsername())
+                    .password(dto.getPassword())
+                    .grantType(grantType)
+                    .build();
+            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(keycloakOAuth, httpComponent.httpHeaders());
+            ResponseEntity<String> response = httpComponent.restTemplate().postForEntity(
+                    keycloakUri + "/protocol/openid-connect/token",
+                    request,
+                    String.class
+            );
+            return response.getBody();
         } catch (Exception e) {
             throw new BadRequestException("Erro ao formatar token - "+e.getMessage());
         }

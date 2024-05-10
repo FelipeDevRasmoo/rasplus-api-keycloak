3d39a887f5bc00802e93d3009d98a817e5b06c0b
Felipe Alves <felipe.barbosa@ext.valid.com>
2024-05-10 00:08:59 -0300

    Set up client authorization

--- src/main/java/com/client/api/rasmooplus/configuration/WebSecurityConfig.java
+++ src/main/java/com/client/api/rasmooplus/configuration/WebSecurityConfig.java
@@ -1,13 +1,25 @@
 package com.client.api.rasmooplus.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @EnableWebSecurity
 @Configuration
@@ -21,24 +33,43 @@ public class WebSecurityConfig {
             "/swagger-resources/**"
     };
 
+    @Value("${keycloak.auth-server-uri}")
+    private String keycloakUri;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(authorize -> {
-                            authorize.requestMatchers( AUTH_SWAGGER_LIST);
-                            authorize.requestMatchers(HttpMethod.GET, "/subscription-type").permitAll();
-                            authorize.requestMatchers(HttpMethod.POST, "/user").permitAll();
-                            authorize.requestMatchers(HttpMethod.POST, "/payment/process").permitAll();
+                            authorize.requestMatchers(AUTH_SWAGGER_LIST);
                             authorize.requestMatchers(HttpMethod.POST, "/auth").permitAll();
-                    authorize.requestMatchers(HttpMethod.POST, "/auth/refresh-token").permitAll();
-                            authorize.requestMatchers("/auth/recovery-code/*").permitAll();
-                            authorize.anyRequest().authenticated();
+                            authorize.requestMatchers(HttpMethod.POST, "/auth/refresh-token").permitAll();
+                            authorize.anyRequest().hasAnyAuthority("CLIENT_READ_WRITE");
                         }
                 )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
 
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(keycloakUri + "/protocol/openid-connect/certs").build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        Converter<Jwt, Collection<GrantedAuthority>> jwtCollectionConverter = jwt-> {
+            Map<String, Object> resourcesAccess = jwt.getClaim("realm_access");
+            Collection<String> roles = (Collection<String>) resourcesAccess.get("roles");
+            return roles.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        };
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtCollectionConverter);
+        return jwtAuthenticationConverter;
     }
 
 }
